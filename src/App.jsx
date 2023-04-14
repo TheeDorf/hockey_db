a064cf94c5128f906c20717ef5128269ba1fc1be
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-14 13:03:40 -0700

    add a time change function

--- src/App.jsx
+++ src/App.jsx
@@ -6,74 +6,101 @@ const LiveScores = () => {
   const [games, setGames] = useState([]);
   const [schedule, setSchedule] = useState([]);
 
-  const fetchGames = async () => {
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "nhl-api5.p.rapidapi.com",
-      },
-    };
+//   const fetchGames = async () => {
+//     const options = {
+//       method: "GET",
+//       headers: {
+//         "X-RapidAPI-Key": API_KEY,
+//         "X-RapidAPI-Host": "nhl-api5.p.rapidapi.com",
+//       },
+//     };
 
-    // Fetch live game scores
-    const liveScoresResponse = await fetch(
-      "https://nhl-api5.p.rapidapi.com/nhlsbox?id=401458986",
-      options
-    );
-    const liveScoresData = await liveScoresResponse.json();
-    if (liveScoresData.games) {
-      setGames(liveScoresData.games);
-    }
+//     // Fetch live game scores
+//     const liveScoresResponse = await fetch(
+//       "https://nhl-api5.p.rapidapi.com/nhlsbox?id=401458986",
+//       options
+//     );
+//     const liveScoresData = await liveScoresResponse.json();
+//     if (liveScoresData.games) {
+//       setGames(liveScoresData.games);
+//     }
 
-    // Fetch NHL schedule
-    const scheduleResponse = await fetch(
-      "https://nhl-api5.p.rapidapi.com/nhlschedule?year=2023&month=04&day=14",
-      options
-    );
-    const scheduleData = await scheduleResponse.json();
+//     // Fetch NHL schedule
+//     const scheduleResponse = await fetch(
+//       "https://nhl-api5.p.rapidapi.com/nhlschedule?year=2023&month=04&day=14",
+//       options
+//     );
+//     const scheduleData = await scheduleResponse.json();
 
-    console.log(schedule);
+//     console.log(schedule);
 
-    setSchedule(scheduleData);
-    // if (scheduleData.dates && scheduleData.dates[0].games) {
-    // }
-  };
+//     setSchedule(scheduleData);
+//     // if (scheduleData.dates && scheduleData.dates[0].games) {
+//     // }
+//   };
+ 
+  
+  let timeHandler = (time) => {
+    let start = time.indexOf("T")
+    let end = time.indexOf("Z")
+    let num = parseInt(time.substring(start, end).replace("T", "").replace(":", ""));
+    console.log(num)
+  if ( num == 0) {
+    num = 2400;
+    console.log(num)
+  }else {num = parseInt(time.substring(start, end).replace("T", "").replace(":", ""))};
+  let temp = (num- 400) > 1200 ? num-400 - 1200 +"pm EST" : num-400+"am EST";
+
+  console.log(temp)
+}
+
+
   useEffect(() => {
-    fetchGames();
+    // fetchGames();
+   timeHandler("2023-04-15T00:00Z")
   }, []);
 
   return (
-    <div className="live-scores-container">
-      {/* //   <h2>Live Scores</h2>
-    //   {games.length > 0 ? (
-    //     games.map((game) => (
-    //       <div key={game.gamePk} className="game-box">
-    //         <p>{game.teams.away.team.name}</p>
-    //         <p>{game.teams.home.team.name}</p>
-    //         <p>{game.livescore.currentPeriodOrdinal}</p>
-    //         <p>
-    //           {game.livescore.teams.away.goals} -{" "}
-    //           {game.livescore.teams.home.goals}
-    //         </p>
-    //       </div>
-    //     ))
-    //   ) : (
-    //     <p>No live games</p>
-    //   )} */}
+//     <div className="live-scores-container">
+//        <h2>Live Scores</h2>
+//       {games.length > 0 ? (
+//         games.map((game) => (
+//           <div key={game.gamePk} className="game-box">
+//             <p>{game.teams.away.team.name}</p>
+//             <p>{game.teams.home.team.name}</p>
+//             <p>{game.livescore.currentPeriodOrdinal}</p>
+//             <p>
+//               {game.livescore.teams.away.goals} -{" "}
+//               {game.livescore.teams.home.goals}
+//             </p>
+//           </div>
+//         ))
+//       ) : (
+//         <p>No live games</p>
+//       )}
 
+//       <h2>NHL Schedule</h2>
+
+//       {schedule[20230414].games.map((game) => {
+        
+//         return (
+//           <div className="game-box">
+//             <p>{game.shortName}</p>
+//             <p> Date & Time: { timeHandler(game.date) } </p>
+//             <p> Period: { game.status.period } </p>
+//           </div>
+//         );
+//       })}
+    <section>
       <h2>NHL Schedule</h2>
-
-      {schedule[20230414].games.map((game) => {
-        return (
-          <div className="game-box">
-            <p>{game.shortName}</p>
-            <p> Date & Time: {game.date - 5} </p>
-            <p> Period: {game.status.period} </p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+      <div className="game-box">
+             <p>gameshortName</p>
+             <p> Date & Time:  </p>
+             <p> Period:  </p>
+           </div>
+    </section>
+//     </div>
+ );
+ };
 
 export default LiveScores;
