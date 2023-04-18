cbc07a1e47cfcfb2dc8bf529018a8b218bad4af6
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-18 10:45:37 -0700

    change api and fix code to pull correctly from new api

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,142 @@
 import React, { useState, useEffect } from "react";
 import background from "./assets/NHL_Logo.png";
 
-const API_KEY = "120dbbf960msh3b1dc96d2563f33p17472djsnc2a87881d164";
+const API_KEY = '120dbbf960msh3b1dc96d2563f33p17472djsnc2a87881d164';
 
 const LiveScores = () => {
-//   const [games, setGames] = useState([]);
-//   const [schedule, setSchedule] = useState([]);
+  const [games, setGames] = useState([]);
+  const [schedule, setSchedule] = useState({});
+  const [isLoading, setIsLoading]= useState(true);
+  const [news, setNews]= useState({})
 
-//   const fetchGames = async () => {
-//     const options = {
-//       method: "GET",
-//       headers: {
-//         "X-RapidAPI-Key": API_KEY,
-//         "X-RapidAPI-Host": "nhl-api5.p.rapidapi.com",
-//       },
-//     };
+  const fetchGames = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "nhl-api5.p.rapidapi.com",
+      },
+    };
 
-//     // Fetch live game scores
-//     const liveScoresResponse = await fetch(
-//       "https://nhl-api5.p.rapidapi.com/nhlsbox?id=401458986",
-//       options
-//     );
-//     const liveScoresData = await liveScoresResponse.json();
-//     if (liveScoresData.games) {
-//       setGames(liveScoresData.games);
-//     }
+    // Fetch live game scores
+    // const liveScoresResponse = await fetch(
+    //   "https://nhl-api5.p.rapidapi.com/nhlscoreboard?year=2023&month=04&day=18&limit=5",
+    //   options
+    // );
+    // const liveScoresData = await liveScoresResponse.json();
+    // if (liveScoresData.games) {
+    //   setGames(liveScoresData.games);
+    // }
 
-//     // Fetch NHL schedule
-//     const scheduleResponse = await fetch(
-//       "https://nhl-api5.p.rapidapi.com/nhlschedule?year=2023&month=04&day=14",
-//       options
-//     );
-//     const scheduleData = await scheduleResponse.json();
 
-//     console.log(schedule);
-
-//     setSchedule(scheduleData);
-//     // if (scheduleData.dates && scheduleData.dates[0].games) {
-//     // }
-//   };
- 
+  fetch(
+    "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard"
+  )
+ .then(response => response.json())
+ .then ((data) => {
+  setSchedule(data)
+  console.log(data)
+ }).catch (err=> console.error(err)).finally(()=> {
+  setIsLoading(false)
+ });
   
-//   let timeHandler = (time) => {
-//     let start = time.indexOf("T")
-//     let end = time.indexOf("Z")
-//     let num = parseInt(time.substring(start, end).replace("T", "").replace(":", ""));
-//     console.log(num)
-//   if ( num == 0) {
-//     num = 2400;
-//     console.log(num)
-//   }else {num = parseInt(time.substring(start, end).replace("T", "").replace(":", ""))};
-//   let temp = (num- 400) > 1200 ? num-400 - 1200 +"pm EST" : num-400+"am EST";
-
-//   console.log(temp)
-// }
-
-
-//   useEffect(() => {
-//  fetchGames();
-//    timeHandler("2023-04-15T00:00Z")
-//   }, []);
-
-//   return (
-//     <div className="live-scores-container" style={{backgroundImage: `url(${background})` }}>
-//        <h2>Live Scores</h2>
-//       {games.length > 0 ? (
-//         games.map((game) => (
-//           <div key={game.gamePk} className="game-box">
-//             <p>{game.teams.away.team.name}</p>
-//             <p>{game.teams.home.team.name}</p>
-//             <p>{game.livescore.currentPeriodOrdinal}</p>
-//             <p>
-//               {game.livescore.teams.away.goals} -{" "}
-//               {game.livescore.teams.home.goals}
-//             </p>
-//           </div>
-//         ))
-
-//       ) : (
-//         <p>No live games</p>
-//       )}
-
-//       <h2>NHL Schedule</h2>
-
-//       {schedule[20230414].games.map((game) => {
-        
-//         return (
-//           <div className="game-box">
-//             <p>{game.shortName}</p>
-//             <p> Date & Time: { timeHandler(game.date) } </p>
-//             <p> Period: { game.status.period } </p>
-//           </div>
-//         );
-//       })}
-//     </div>
-//  );
-return (
-
-<div className="full-container">
-  <img src={background} alt="NHL LOGO" />
-  <div className="card">
-<h2 class="title">Live Scores</h2>
-<div className="live-box">
-<section>
-<p>Home team</p>
-<p>Away team</p>
-<p>Period: #</p>
-<p>Home team Goals #</p> 
-<p>Home team Goals #</p> 
-</section>
-</div>
-
-<section>
-<h2 class="title">NHL Schedule</h2>
-<div className="game-box">
- <p>gameshortName</p>
-<p> Date & Time:  </p>
- <p> Period:  </p>
-</div>
-</section>
-</div>  
-</div>
-
+ fetch(
+  "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/news"
 )
+.then(response => response.json())
+.then ((news) => {
+setNews(news)
+console.log(news)
+}).catch (err=> console.error(err)).finally(()=> {
+setIsLoading(false)
+});
+  
+  };
+  
+  
+  let timeHandler = (time) => {
+    console.log(time)
+    let start = time.indexOf("T")
+    let end = time.indexOf("Z")
+    let num = parseInt(time.substring(start, end).replace("T", "").replace(":", ""));
+    
+  if ( num == 0) {
+    num = 2400;
+    
+  }
+  let temp = (num- 400) > 1200 ? num-400 - 1200 +"pm EST" : num-400+"am EST";
+
+let utcDate = new Date(new Date(time).toISOString().replace("Z", "+0000")).toString()
+
+ 
+  return utcDate
+}
+
+
+  useEffect(() => {
+ fetchGames();
+   timeHandler("2023-04-15T00:00Z")
+  }, []);
+  console.log("test",games)
+  if (isLoading)
+  {return <h1>Loading</h1>
+    
+  }
+  
+  return (
+    <div className="live-scores-container">
+      <img src={background} alt="NHL LOGO" />
+      <div className="card">
+      <h2>NHL Schedule</h2>
+      {schedule.events.map((game) => {
+        
+        return (
+          <div className="game-box">
+            <p>{ game.name }</p>
+            <p> Date & Time: { timeHandler (game.date) } </p>
+            <p> Odds: { game.competitions[0].odds[0].details } </p>
+            <p> Over/Under: { game.competitions[0].odds[0].overUnder } </p>
+            <p> Time: { game.status.displayClock } </p>
+            <p> Period: { game.status.period } </p>
+         </div>
+
+        ); 
+        
+      
+      })};
+    
+      
+      </div>
+    </div>
+ );
+// return (
+
+// <div className="full-container">
+//   <img src={background} alt="NHL LOGO" />
+//   <div className="card">
+// <h2 class="title">Live Scores</h2>
+// <div className="live-box">
+// <section>
+// <p>Home team</p>
+// <p>Away team</p>
+// <p>Period: #</p>
+// <p>Home team Goals #</p> 
+// <p>Home team Goals #</p> 
+// </section>
+// </div>
+
+// <section>
+// <h2 class="title">NHL Schedule</h2>
+// <div className="game-box">
+//  <p>gameshortName</p>
+// <p> Date & Time:  </p>
+//  <p> Period:  </p>
+// </div>
+// </section>
+// </div>  
+// </div>
+
+// )
 }
 
 export default LiveScores;
